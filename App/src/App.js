7369a2ebe786eb417bf82e93d18e3de03d58cd0b
KarmenM18 <karmenjot.minhas@gmail.com>
2024-08-21 23:18:12 -0400

    centered leaderboard page

--- App/src/App.js
+++ App/src/App.js
@@ -5,8 +5,9 @@ import QuizPage from './QuizPage/QuizPage';
 import QuizPage2 from './QuizPage2/QuizPage2';
 import TasksPage from './TasksPage/TasksPage';
 import HomePage from './HomePage/HomePage';
-import Leaderboard from './LeaderboardPage/leaderboard';
+import Leaderboard from './LeaderboardPage/board';
 import myBadges from './BadgesPage/myBadges';
+//import './LeaderboardPage/styles.css';
 
 function App() {
   return (
@@ -18,7 +19,14 @@ function App() {
         <Route path='/' element={<QuizPage/>}></Route>
         <Route path='/quiz2' element={<QuizPage2/>}></Route>
         <Route path='/tasks' element={<TasksPage/>}></Route>
-        <Route path='/leaderboard' element={<Leaderboard/>}></Route>
+        <Route
+          path='/leaderboard'
+          element={
+            <div className="App" id='main'>
+              <Leaderboard />
+            </div>
+          }
+        />
       </Routes>
     </Router>
   );
